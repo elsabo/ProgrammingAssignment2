1eaa11a3ad872d3a79575b418ea2de2575e2b537
elsabo <elsabo@gmail.com>
2015-02-13 17:50:42 -0600

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-  ix <- NULL
-
-  get <- function() x
-  set <- function(m)  { 
-    x <<- m
-    ix <<- NULL
-  }
-  
-  inverse <- function() ix
-  invert <- function() {
-    ix <<- solve(get())
-  }
-  list(get = get, set = set, inverse = inverse, invert = invert)
-}
-
 ## hilbert function copied from ?solve
 hilbert <- function(n) { 
   i <- 1:n
@@ -26,16 +8,45 @@ hilbert <- function(n) {
 }
 
 ## Write a short comment describing this function
+## Recieves x as matrix, it has to be inversable, no error handling added
+## inverse: returns the value of ix, defined to hold the inverse of the instatiated matrix
+## invert: Will inverse matrix x and store it in ix
+makeCacheMatrix <- function(x = matrix()) {
+  ix <- NULL
+
+  get <- function() x
+  set <- function(m)  { 
+    ## set can be used to change the matrix
+    x <<- m
+    ix <<- NULL
+  }
+  
+  inverse <- function() ix
+  invert <- function() {
+    ## Extremely important to pay attention to the lexical scoping 
+    ix <<- solve(get())
+  }
+  list(get = get, set = set, inverse = inverse, invert = invert)
+}
+
+
+## Write a short comment describing this function
+## Receives a special matrix created with makeCacheMatrix
+## makeCacheMatrix holds an invertable matrix 
+## It will always return the inverse of the matrix stored in the object makeCacheMatrix
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   
+  ##Check if we already have the inverse
   inverted <- x$inverse()
   
   if (is.null(inverted)) {
+    ## We do not
     message("First time, lets cache this")
     x$invert()
     inverted <- x$inverse()
   } else {
+    ## We do
     message("Got it from the cache")
   }
   
