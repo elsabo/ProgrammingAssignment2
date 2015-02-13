8c678fe26a6912e602d140a1f106f400d375347e
elsabo <elsabo@gmail.com>
2015-02-13 17:36:46 -0600

    Coursera Assigment Done

--- README.md
+++ README.md
@@ -103,3 +103,18 @@ In order to complete this assignment, you must do the following:
 ### Grading
 
 This assignment will be graded via peer assessment.
+
+
+### Usage
+Additional to the `makeCacheMatrix` and the `cacheSolve` functions you will find the `hilbert` function
+which it would generate a random matrix of size n by n
+ie. 
+>hilbert(6) 
+will create a 6 by 6 matrix Hilbert matrix that can be used by  `makeCacheMatrix` and `cacheSolve`
+
+Sample run
+
+    m = hilbert(6)
+    mcached = makeCacheMatrix(m)
+    cacheSolve(mcached)
+    
--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  ix <- NULL
 
+  get <- function() x
+  set <- function(m)  { 
+    x <<- m
+    ix <<- NULL
+  }
+  
+  inverse <- function() ix
+  invert <- function() {
+    ix <<- solve(get())
+  }
+  list(get = get, set = set, inverse = inverse, invert = invert)
 }
 
+## hilbert function copied from ?solve
+hilbert <- function(n) { 
+  i <- 1:n
+  1 / outer(i - 1, i, "+") 
+}
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  inverted <- x$inverse()
+  
+  if (is.null(inverted)) {
+    message("First time, lets cache this")
+    x$invert()
+    inverted <- x$inverse()
+  } else {
+    message("Got it from the cache")
+  }
+  
+  inverted
 }
+
